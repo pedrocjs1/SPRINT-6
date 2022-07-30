d4d6998a3ebd73f23e31dce93f559d0b348885c7
Karen Lettieri <karulettieri2109@gmail.com>
2022-07-29 22:27:39 -0300

    update del archivo update

--- update.sql
+++ update.sql
@@ -1,13 +1,18 @@
--- PRUEBA DE CARGA DE DATOS PARA CORREGIR FECHA DE EMPLOYEE_HIRE_DATE 
+-- #### PRUEBA DE CARGA DE DATOS PARA CORREGIR FECHA DE EMPLOYEE_HIRE_DATE 
 
 --INSERT INTO empleado (employee_id, employee_name, employee_surname, employee_hire_date, employee_dni, branch_id) values (1, 'Juancito', 'Perez', '29/07/2022', '23192755', 2) 
 
 --UPDATE empleado 
 --SET employee_hire_date =  ('29/07/2022')
-
 --SELECT * from empleado
+
+-- #### DELETE DE LOS DATOS CARGADOS ANTERIORMENTE
+--DELETE FROM empleado
+--WHERE employee_id = 1;
 
 UPDATE empleado
 SET employee_hire_date = (substr(employee_hire_date, 7, 4) || '-' || substr(employee_hire_date, 4, 2) || '-' || substr(employee_hire_date, 1, 2)) 
 WHERE employee_hire_date <> ''
+
+
 
